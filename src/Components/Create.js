c7276302faf2217bdffb1a42f7a50b3cc9b1d0f3
Desire <desireirankundawilliams@gmail.com>
2022-08-02 21:07:55 +0200

    able to save the data to firebase

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,11 +1,15 @@
 import { useColorModeValue, useColorMode, FormLabel } from '@chakra-ui/react';
-import React, { useState } from 'react'
-import { Flex, Input, Menu, MenuButton, MenuList, MenuItem, Button, Text, InputGroup, InputLeftElement, Box } from '@chakra-ui/react';
-import { IoChevronDown, IoCloudUpload, IoLocation,IoTrash } from 'react-icons/io5';
+import React, { useState, useEffect } from 'react'
+import { Flex, Input, Menu, MenuButton, MenuList, MenuItem, Button, Text, InputGroup, InputLeftElement, Textarea, Box } from '@chakra-ui/react';
+import { IoCheckmark, IoChevronDown, IoCloudUpload, IoLocation, IoTrash, IoWarning } from 'react-icons/io5';
 import { categories } from '../data';
 import Spinner from './Spinner';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL, deleteObject } from 'firebase/storage'
 import { firebaseApp } from '../firebase';
+import AlertMessage from './AlertMessage';
+import { fetchUserInfo } from "../Utils/FetchUser"
+import { setDoc, getFirestore, doc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
   const { colorMode } = useColorMode();
@@ -17,9 +21,18 @@ const Create = () => {
   const [category, setCategory] = useState('Choose category')
   const [videoAsset, setVideoAsset] = useState(null)
   const [loading, setLoading] = useState(false)
-  const [progress, setProgress] = useState(50)
+  const [progress, setProgress] = useState(0)
+  const [alert, setAlert] = useState(false)
+  const [alertStatus, setAlertStatus] = useState('')
+  const [alertMsg, setAlertMsg] = useState('')
+  const [alertIcon, setAlertIcon] = useState(null)
+  const [description, setDescription] = useState('')
+
+  const [userInfo] = fetchUserInfo()
+  const navigate = useNavigate()
 
   const storage = getStorage(firebaseApp)
+  const firebaseDb = getFirestore(firebaseApp)
 
   const uploadImage = (e) => {
     setLoading(true)
@@ -35,9 +48,12 @@ const Create = () => {
       console.log(error);
     }, () => {
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        setVideoAsset(downloadURL)
-        setLoading(false)
-        console.log('downloadurl', downloadURL);
+        setVideoAsset(downloadURL);
+        setLoading(false);
+        // setAlert(true);
+        setAlertStatus('Success');
+        setAlertIcon(<IoCheckmark fontSize={25} />);
+        setAlertMsg('Your video is uploaded');
       })
 
     })
@@ -47,10 +63,52 @@ const Create = () => {
     const deleteRef = ref(storage, videoAsset)
     deleteObject(deleteRef).then(() => {
       setVideoAsset(null)
+      setAlert(true)
+      setAlertStatus('error')
+      setAlertIcon(<IoWarning fontSize={25} />)
+      setAlertMsg('Your video was removed')
+      setTimeout(() => {
+        setAlert(false)
+      }, 4000)
     }).catch((error) => {
       console.log(error);
     })
   }
+
+  const uploadDetails = async () => {
+    try {
+      setLoading(true)
+      if (!title && !category && !videoAsset) {
+        setAlert(true)
+        setAlertStatus('error')
+        setAlertIcon(<IoWarning fontSize={25} />)
+        setAlertMsg('Required fields are missing!')
+        setTimeout(() => {
+          setAlert(false)
+        }, 4000)
+        setLoading(false)
+      } else {
+        const data = {
+          id: `${Date.now()}`,
+          title: title,
+          userId: userInfo?.uid,
+          category: category,
+          location: location,
+          videoUrl: videoAsset,
+          description: description
+        }
+        await setDoc(doc(firebaseDb, 'videos', `${Date.now()}`), data)
+        setLoading(false)
+        navigate('/', { replace: true })
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+
+  }, [videoAsset])
 
 
   return (
@@ -74,6 +132,10 @@ const Create = () => {
         justifyContent={'center'}
         gap={2}
       >
+
+        {alert && (
+          <AlertMessage status={alertStatus} msg={alertMsg} icon={alertIcon} />
+        )}
         <Input variant='flushed' placeholder='Title'
           focusBorderColor="gray.400"
           isRequired
@@ -180,38 +242,61 @@ const Create = () => {
               )
             }
           </FormLabel> : (
-          <Flex 
+            <Flex
+              width={'full'}
+              height='full'
+              justifyContent={'center'}
+              alignItems='center'
+              bg="black"
+              position={'relative'}
+            >
+              <Flex
+                width={'40px'}
+                height='40px'
+                justifyContent={'center'}
+                alignItems='center'
+                bg="red"
+                position={'absolute'}
+                top={5}
+                right={5}
+                cursor={"pointer"}
+                zIndex={10}
+                rounded='full'
+                onClick={deleteVideo}
+              >
+                <IoTrash fontSize={20} color='white' />
+              </Flex>
+              <video
+                src={videoAsset}
+                controls
+                style={{ width: '100%', height: '100%' }}
+              />
+            </Flex>
+          )}
+        </Flex>
+        <Flex
           width={'full'}
           height='full'
           justifyContent={'center'}
           alignItems='center'
-          bg="black"
           position={'relative'}
-          > 
-           <Flex 
-          width={'40px'}
-          height='40px'
-          justifyContent={'center'}
-          alignItems='center'
-          bg="red"
-          position={'absolute'}
-          top={5}
-          right={5}
-          cursor={"pointer"}
-          zIndex={10}
-          rounded='full'
-          onClick={deleteVideo}
-          > 
-          <IoTrash fontSize={20} color='white' /> 
-          </Flex>
-          <video
-          src={videoAsset}
-          controls
-          style={{width: '100%', height: '100%'}}
-          /> 
-          </Flex>
-          )}
+        >
+          <Textarea
+            placeholder='Enter the video description'
+            size='sm'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </Flex>
+        <Button isLoading={loading} loadingText='uploading' colorScheme={"linkedin"}
+          variant={`${loading ? 'outline' : 'solid'}`}
+          width={'xl'}
+          _hover={{ shadow: 'lg' }}
+          fontSize={20}
+          onClick={() => uploadDetails()}
+        >
+          Upload
+        </Button>
       </Flex>
     </Flex>
   )
