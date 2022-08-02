a0f28c4c786297fe60740c484632d4be2a4681cb
Desire <desireirankundawilliams@gmail.com>
2022-08-02 10:37:43 +0200

    created a sigin button for google

--- src/Container/Login.js
+++ src/Container/Login.js
@@ -1,6 +1,7 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Button, Flex, HStack, Image } from '@chakra-ui/react'
 import React from 'react'
 import background from '../Images/student.jpeg'
+import {FcGoogle} from 'react-icons/fc'
 
 const Login = () => {
     return (
@@ -16,6 +17,23 @@ const Login = () => {
                 width={"full"}
                 height={"full"}
             />
+            <Flex
+            position={"absolute"}
+            width={"100vw"}
+            height={"100vh"}
+            bg={"backAlpha.600"}
+            top={0}
+            left={0}
+            justifyContent="center"
+            alignItems={"center"}
+
+>
+            <HStack>
+                <Button leftIcon={<FcGoogle fontSize={25} />} colorScheme="whiteAlpha" shadow={"lg"} >
+                    Sign in with Google
+                </Button>
+            </HStack>
+            </Flex>
         </Flex>
     )
 }
