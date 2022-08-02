58eb8ed0e8a454bde78dfd1165fac69223017737
Desire <desireirankundawilliams@gmail.com>
2022-08-02 12:23:45 +0200

    created the main routes

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Flex } from '@chakra-ui/react';
+import { Category, NavBar, Feed, Create, VideoPin,Search } from '../Components/Index';
+import { Routes, Route } from "react-router-dom";
 
-const Home = ({user}) => {
-    console.log('user',user);
-  return (
-    
-    <div>Home</div>
-  )
+const Home = ({ user }) => {
+    console.log('user', user);
+    return (
+        <>
+            <NavBar user={user} />
+            <Flex
+                direction={'column'}
+                justifyContent="start"
+                alignItems={"center"}
+                width="20"
+            >
+                <Category />
+            </Flex>
+            <Flex
+                width={'full'}
+                justifyContent='center'
+                alignItems={'center'}
+                px='4'
+            >
+                <Routes>
+                    <Route path='/' element={<Feed />} />
+                    <Route path='/category/:categoryId' element={<Feed />} />
+                    <Route path='/create' element={<Create />} />
+                    <Route path='/videoDetail/:videoId' element={<VideoPin />} />
+                    <Route path='/search' element={<Search />} />
+                </Routes>
+            </Flex>
+        </>
+    )
 }
 
 export default Home
