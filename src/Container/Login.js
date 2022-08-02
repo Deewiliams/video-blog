4b5e4f7127f77bfe1171989082ea00247e68c855
Desire <desireirankundawilliams@gmail.com>
2022-08-02 10:24:32 +0200

    added and background image

--- src/Container/Login.js
+++ src/Container/Login.js
@@ -1,9 +1,23 @@
- import React from 'react'
- 
- const Login = () => {
-   return (
-     <div>Login</div>
-   )
- }
- 
- export default Login
+import { Flex, Image } from '@chakra-ui/react'
+import React from 'react'
+import background from '../Images/student.jpeg'
+
+const Login = () => {
+    return (
+        <Flex
+            justifyContent={"center"}
+            alignItems={"center"}
+            width={"100vw"}
+            height={"100vh"}
+            position={"relative"}
+        >
+            <Image src={background}
+                objectFit="cover"
+                width={"full"}
+                height={"full"}
+            />
+        </Flex>
+    )
+}
+
+export default Login
