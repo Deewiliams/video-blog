274f2ab28152b786f6a4580f6c00bd6b23bc753e
Desire <desireirankundawilliams@gmail.com>
2022-08-02 13:13:32 +0200

    created a add button

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,26 +1,68 @@
 import React from 'react'
 import logo from '../Images/logo.png'
 import { useNavigate, Link } from 'react-router-dom'
-import { useColorModeValue,useColorMode } from '@chakra-ui/react'
-import { Flex,Image } from '@chakra-ui/react'
+import { useColorModeValue, useColorMode } from '@chakra-ui/react'
+import { Flex, Image, InputGroup, InputLeftElement, Input } from '@chakra-ui/react'
+import { IoAdd, IoMoon, IoSearch, IoSunny } from 'react-icons/io5'
 
 
-const NavBar = ({user}) => {
+const NavBar = ({ user }) => {
     const { colorMode, toggleColorMode } = useColorMode();
 
     const bg = useColorModeValue("gray.600", "gray.300");
-  return (
-    <Flex
-    justifyContent={'space-between'}
-    alignItems='center'
-    width={'100vw'}
-    p={4}
-    >
-<Link to={'/'} >
-<Image src={colorMode == 'light ' ? 'vision creation' : logo} width={'180px'} /> 
-</Link>
-    </Flex>
-  )
+    return (
+        <Flex
+            justifyContent={'space-between'}
+            alignItems='center'
+            width={'100vw'}
+            p={4}
+        >
+            <Link to={'/'} >
+                <Image src={colorMode == 'light ' ? 'vision creation' : logo} width={'180px'} />
+            </Link>
+
+            <InputGroup mx={6} width='60vw'>
+                <InputLeftElement
+                    pointerEvents='none'
+                    children={<IoSearch fontSize={25} />}
+                />
+                <Input type='text' placeholder='Search...'
+                    fontSize={18}
+                    fontWeight='medium'
+                    varient={"filled"}
+                />
+            </InputGroup>
+
+            <Flex justifyContent={'center'} alignItems='center'>
+                <Flex
+                    width={"40px"}
+                    height="40px"
+                    alignItems='center'
+                    cursor={'pointer'}
+                    borderRadius='5px'
+                    onClick={toggleColorMode}
+                >
+                    {colorMode == 'light' ? (<IoMoon fontSize={25} />) : (<IoSunny fontSize={25} />)}
+                </Flex>
+
+                <Link to={'/create'}>
+                    <Flex justifyContent={"center"}
+                        alignItems="center" bg={bg}
+                        width="40px"
+                        height="40px"
+                        borderRadius='5px'
+                        mx={6}
+                        cursor='pointer'
+                        _hover={{ shadow: 'md'}}
+                        transition="ease-in-out"
+                        transitionDuration={"0.3s"}
+                         >
+                        <IoAdd fontSize={25} color={`${colorMode == 'dark' ? '#111' : '#f1f1f1'}`} />
+                    </Flex>
+                </Link>
+            </Flex>
+        </Flex>
+    )
 }
 
 export default NavBar
