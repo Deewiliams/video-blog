cd7d5f100c79a579ffe1703f8ef2a106b9c96673
Desire <desireirankundawilliams@gmail.com>
2022-08-02 12:38:38 +0200

    added a navbar logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import logo from '../Images/logo.png'
+import { useNavigate, Link } from 'react-router-dom'
+import { useColorModeValue,useColorMode } from '@chakra-ui/react'
+import { Flex,Image } from '@chakra-ui/react'
 
-const NavBar = () => {
+
+const NavBar = ({user}) => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    const bg = useColorModeValue("gray.600", "gray.300");
   return (
-    <div>NavBar</div>
+    <Flex
+    justifyContent={'space-between'}
+    alignItems='center'
+    width={'100vw'}
+    p={4}
+    >
+<Link to={'/'} >
+<Image src={colorMode == 'light ' ? 'vision creation' : logo} width={'180px'} /> 
+</Link>
+    </Flex>
   )
 }
 
