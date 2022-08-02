9c7f416d941672c5435328d808d38074f92434ca
Desire <desireirankundawilliams@gmail.com>
2022-08-02 13:34:17 +0200

    created a logout button

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,13 +2,13 @@ import React from 'react'
 import logo from '../Images/logo.png'
 import { useNavigate, Link } from 'react-router-dom'
 import { useColorModeValue, useColorMode } from '@chakra-ui/react'
-import { Flex, Image, InputGroup, InputLeftElement, Input } from '@chakra-ui/react'
-import { IoAdd, IoMoon, IoSearch, IoSunny } from 'react-icons/io5'
+import { Flex, Image, InputGroup, InputLeftElement, Input, MenuButton, MenuList, Menu, MenuItem, Button } from '@chakra-ui/react'
+import { IoAdd, IoLogOut, IoMoon, IoSearch, IoSunny } from 'react-icons/io5'
 
 
 const NavBar = ({ user }) => {
     const { colorMode, toggleColorMode } = useColorMode();
-
+    console.log('user', user?.photoURL);
     const bg = useColorModeValue("gray.600", "gray.300");
     return (
         <Flex
@@ -53,13 +53,34 @@ const NavBar = ({ user }) => {
                         borderRadius='5px'
                         mx={6}
                         cursor='pointer'
-                        _hover={{ shadow: 'md'}}
+                        _hover={{ shadow: 'md' }}
                         transition="ease-in-out"
                         transitionDuration={"0.3s"}
-                         >
+                    >
                         <IoAdd fontSize={25} color={`${colorMode == 'dark' ? '#111' : '#f1f1f1'}`} />
                     </Flex>
                 </Link>
+
+                <Menu>
+                    <MenuButton>
+                        <Image src={user?.photoURL}
+                            width='40px'
+                            height='40px'
+                            rounded="full" />
+                    </MenuButton>
+                    <MenuList>
+                        <Link to={''}>
+                            <MenuItem>My Account</MenuItem>
+                        </Link>
+                        <MenuItem 
+                        flexDirection={'row '}
+                        alignItems="center"
+                        gap={4}
+                        >Logout <IoLogOut fontSize={20} /> </MenuItem>
+                    </MenuList>
+                </Menu>
+
+
             </Flex>
         </Flex>
     )
